2c6d0df4847946f1cad460ed740bc25f6a5b7423
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2021-12-13 13:14:10 +0530

    Finished JS to control Nav Collapse

---
+++ main.js
@@ -0,0 +1,7 @@
+const navIcon = document.querySelector("#nav-icon");
+const navItemsContainer = document.querySelector("#nav-items-container");
+
+// Function to toggle Display of Nav Items
+navIcon.addEventListener("click", e => {
+  navItemsContainer.classList.toggle("display");
+})
