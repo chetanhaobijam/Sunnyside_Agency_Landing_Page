e6840f01276ddc9e0a3448c335b351ae5515fdc3
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2022-01-17 13:37:41 +0530

    +Custom ScrollBar +ScrollToTop

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const navIcon = document.querySelector("#nav-icon");
 const navItemsContainer = document.querySelector("#nav-items-container");
+const scrollToTopBtn = document.querySelector(".scrollTop");
+const rootElement = document.documentElement;
 
 // Function to toggle Display of Nav Items
 navIcon.addEventListener("click", (e) => {
   navItemsContainer.classList.toggle("display");
 });
+
+// Function for Scroll To Top Button
+const handleScroll = () => {
+  const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > 0.2) {
+    scrollToTopBtn.classList.add("display");
+  } else {
+    scrollToTopBtn.classList.remove("display");
+  }
+};
+
+document.addEventListener("scroll", handleScroll);
